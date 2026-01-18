9b1166a19a2f0ad373eac972c6934f215dfce41f
Seapeas <dodinhhai29904@gmail.com>
2026-01-18 22:50:03 +0700

    feat: add Swagger documentation setup and dependencies

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,15 +6,27 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  const config = new DocumentBuilder()
+    .setTitle('JoblyAI API')
+    .setDescription('The JoblyAI backend API description')
+    .setVersion('1.0')
+    .addBearerAuth() // Optional: Useful if you add JWT auth later
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `📚 Swagger documentation is running on: http://localhost:${port}/api/docs`
   );
 }
 
