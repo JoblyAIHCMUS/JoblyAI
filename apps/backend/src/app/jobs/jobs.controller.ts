ed7bd1cf94cbcb0ceb4164e96acb7c93b9e92a50
MH221B <22127131@student.hcmus.edu.vn>
2026-02-13 21:39:29 +0700

    feat: add createJob endpoint with authentication and user association

--- apps/backend/src/app/jobs/jobs.controller.ts
+++ apps/backend/src/app/jobs/jobs.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import type { Request } from 'express';
+import { Body, Controller, Get, Post, Query, UseGuards, Req } from "@nestjs/common";
 import { JobsService } from "./jobs.service";
 import { GetJobsQueryDTO } from "./dto/getJobsQueryDTO";
+import { AuthGuard } from "../auth/auth.guard";
+import { CreateJobDto } from "./dto/createJobDTO";
+
+interface RequestWithUser extends Request {
+    user: {
+        id: string;
+    }
+}
 
 @Controller("jobs")
 export class JobsController{
@@ -9,5 +18,12 @@ export class JobsController{
     @Get()
     async getJobs(@Query() query: GetJobsQueryDTO) {
         return this.jobsService.getsPaginatedJobsPostings(query);
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    async createJob(@Body() createJobDto: CreateJobDto, @Req() req: RequestWithUser) {
+        const userId = req.user.id;
+        return this.jobsService.createJob(createJobDto, userId);
     }
 }
