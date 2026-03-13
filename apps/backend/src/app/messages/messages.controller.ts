ee358a529b1a60e912cb4c0338212a9df26ec2ce
MH221B <22127131@student.hcmus.edu.vn>
2026-03-13 10:35:05 +0700

    chore: format code

--- apps/backend/src/app/messages/messages.controller.ts
+++ apps/backend/src/app/messages/messages.controller.ts
@@ -5,38 +5,42 @@ import type { AuthenticatedRequest } from '../types/authenticatedRequest';
 
 @Controller('chats')
 export class MessagesController {
-    constructor(private readonly messagesService: MessagesService) {}
+  constructor(private readonly messagesService: MessagesService) {}
 
-    @Get('summary')
-    async getChatListSummary(
-        @Query('userId') userId: string
-    ): Promise<ChatStatusResponse[]> {
-        return this.messagesService.getChatListSummary(userId);
-    }
+  @Get('summary')
+  async getChatListSummary(
+    @Query('userId') userId: string
+  ): Promise<ChatStatusResponse[]> {
+    return this.messagesService.getChatListSummary(userId);
+  }
 
-    @Get('history/:friendId')
-    async getHistory(
-        @Request() req: AuthenticatedRequest, 
-        @Param('friendId') friendId: string,
-        @Query('limit') limit?: number
-    ) {
-        return await this.messagesService.getChatHistory(req.user.id, friendId, limit || 50);
-    }
+  @Get('history/:friendId')
+  async getHistory(
+    @Request() req: AuthenticatedRequest,
+    @Param('friendId') friendId: string,
+    @Query('limit') limit?: number
+  ) {
+    return await this.messagesService.getChatHistory(
+      req.user.id,
+      friendId,
+      limit || 50
+    );
+  }
 
-    @Post('read/:friendId')
-    async markAsRead(
-        @Request() req: AuthenticatedRequest,
-        @Param('friendId') friendId: string
-    ) {
-        return await this.messagesService.markAsRead(req.user.id, friendId);
-    }
+  @Post('read/:friendId')
+  async markAsRead(
+    @Request() req: AuthenticatedRequest,
+    @Param('friendId') friendId: string
+  ) {
+    return await this.messagesService.markAsRead(req.user.id, friendId);
+  }
 
-    @Post('init/:friendId')
-    async initChat(
-        @Request() req: AuthenticatedRequest, 
-        @Param('friendId') friendId: string
-    ) {
-        await this.messagesService.createConversation(req.user.id, friendId);
-        return { success: true };
-    }
+  @Post('init/:friendId')
+  async initChat(
+    @Request() req: AuthenticatedRequest,
+    @Param('friendId') friendId: string
+  ) {
+    await this.messagesService.createConversation(req.user.id, friendId);
+    return { success: true };
+  }
 }
