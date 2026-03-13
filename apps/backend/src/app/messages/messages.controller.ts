e9a0f1f2428e0bb2ebd4ed141ca8b1a39a433efa
MH221B <22127131@student.hcmus.edu.vn>
2026-03-13 10:24:51 +0700

    feat: add MessagesModule and MessagesController for chat functionality

---
+++ apps/backend/src/app/messages/messages.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { MessagesService } from './messages.service';
+import { ChatStatusResponse } from './messages.interface';
+
+@Controller('chats')
+export class MessagesController {
+  constructor(private readonly messagesService: MessagesService) {}
+  
+  @Get('summary')
+  async getChatListSummary(
+    @Query('userId') userId: string
+  ): Promise<ChatStatusResponse[]> {
+    return this.messagesService.getChatListSummary(userId);
+  }
+}
