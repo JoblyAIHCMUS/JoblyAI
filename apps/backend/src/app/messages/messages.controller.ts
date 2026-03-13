1a1f43d55d55dd7afdc71b049f0aca04f2188597
MH221B <22127131@student.hcmus.edu.vn>
2026-03-13 10:27:16 +0700

    chore: refactor code

--- apps/backend/src/app/messages/messages.controller.ts
+++ apps/backend/src/app/messages/messages.controller.ts
@@ -5,7 +5,7 @@ import { ChatStatusResponse } from './messages.interface';
 @Controller('chats')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
-  
+
   @Get('summary')
   async getChatListSummary(
     @Query('userId') userId: string
