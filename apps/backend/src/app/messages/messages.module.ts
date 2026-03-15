44fc7e263210259c4ec741b511bf83d1094edaa9
MH221B <22127131@student.hcmus.edu.vn>
2026-03-15 20:42:59 +0700

    feat: add AuthModule to MessagesModule imports

--- apps/backend/src/app/messages/messages.module.ts
+++ apps/backend/src/app/messages/messages.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { MessagesGateway } from './messages.gateway';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [MessagesController],
   providers: [MessagesService, MessagesGateway],
 })
