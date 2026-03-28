8070128950ec9ca37cc97640e250586e3d8871b4
Dat Thanh Pham <phamdat5914378@gmail.com>
2026-03-28 18:32:16 +0700

    fix fe base on new job res

--- apps/web/src/api-client/jobs/employer.ts
+++ apps/web/src/api-client/jobs/employer.ts
@@ -6,8 +6,7 @@ import {
   JobCategory,
 } from '@/api-client/jobs/types';
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
 /**
  * Get all job categories
