ebea2693e2d2f06efa79871b1f997e91958afcaa
HoangRory <127863890+HoangRory@users.noreply.github.com>
2026-04-03 23:51:56 +0700

    refactor: remove unused columns and tables from migration for personal details

--- apps/backend/prisma/migrations/20260403125347_add_personal_details_fields/migration.sql
+++ apps/backend/prisma/migrations/20260403125347_add_personal_details_fields/migration.sql
@@ -2,21 +2,6 @@
 CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'OTHER');
 
 -- AlterTable
-ALTER TABLE "Experience" ADD COLUMN     "type" TEXT;
-
--- AlterTable
 ALTER TABLE "user" ADD COLUMN     "dateOfBirth" TIMESTAMP(3),
 ADD COLUMN     "gender" "Gender",
 ADD COLUMN     "phoneNumber" TEXT;
-
--- CreateTable
-CREATE TABLE "CandidateDescription" (
-    "id" SERIAL NOT NULL,
-    "candidateId" TEXT NOT NULL,
-    "title" TEXT,
-    "bio" TEXT,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "CandidateDescription_pkey" PRIMARY KEY ("id")
-);
