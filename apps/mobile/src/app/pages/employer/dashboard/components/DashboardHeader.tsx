43d3bf778c401a3fb2a43711245489f331ccd9cd
MH221B <22127131@student.hcmus.edu.vn>
2026-05-17 16:05:44 +0700

    style: update font sizes in DashboardHeader for consistency

--- apps/mobile/src/app/pages/employer/dashboard/components/DashboardHeader.tsx
+++ apps/mobile/src/app/pages/employer/dashboard/components/DashboardHeader.tsx
@@ -5,10 +5,10 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 export const DashboardHeader = () => {
   return (
     <View className="px-6 pt-10 pb-6">
-      <Text className="text-[30px] font-extrabold text-[#0F172A] mb-1">
+      <Text className="text-3xl font-extrabold text-[#0F172A] mb-1">
         Good morning, Maria
       </Text>
-      <Text className="text-[18px] text-[#475569] mb-2 leading-6">
+      <Text className="text-lg text-[#475569] mb-2 leading-6">
         Here is your job listings statistic report from July 19 - July 25.
       </Text>
       
@@ -16,7 +16,7 @@ export const DashboardHeader = () => {
         activeOpacity={0.7}
         className="flex-row items-center justify-between border border-[#CBD5E1] rounded-xl px-4 py-4 bg-white"
       >
-        <Text className="text-[18px] font-medium text-[#0F172A]">
+        <Text className="text-lg font-medium text-[#0F172A]">
           Jul 19 - Jul 25
         </Text>
         <MaterialCommunityIcons name="calendar" size={24} color="#4338CA" />
