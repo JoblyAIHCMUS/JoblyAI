73c1c2ea888df7c365220b95ed4c7c594bb09b78
MH221B <150142930+MH221B@users.noreply.github.com>
2026-07-18 09:41:38 +0700

    fix: increase max length for questions and expected answers to 10000 characters (#278)
    * fix: increase max length for questions and expected answers to 10000 characters
    
    * chore: format file
    
    * fix: update dependencies
    
    * chore: format files

--- apps/backend/src/app/jobs/dto/preShortlistQuestionInput.ts
+++ apps/backend/src/app/jobs/dto/preShortlistQuestionInput.ts
@@ -3,15 +3,17 @@ import { IsString, MinLength, MaxLength } from 'class-validator';
 export class PreShortlistQuestionInput {
   @IsString()
   @MinLength(5, { message: 'Each question must be at least 5 characters' })
-  @MaxLength(500, { message: 'Each question must be at most 500 characters' })
+  @MaxLength(10000, {
+    message: 'Each question must be at most 10000 characters',
+  })
   question!: string;
 
   @IsString()
   @MinLength(1, {
     message: 'Each expected answer must be at least 1 character',
   })
-  @MaxLength(500, {
-    message: 'Each expected answer must be at most 500 characters',
+  @MaxLength(10000, {
+    message: 'Each expected answer must be at most 10000 characters',
   })
   expectedAnswer!: string;
 }
