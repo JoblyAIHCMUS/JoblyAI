b2d141e498bee3202b4368a4e201d1a032401730
MH221B <22127131@student.hcmus.edu.vn>
2026-07-18 09:24:22 +0700

    chore: format file

--- apps/backend/src/app/jobs/dto/preShortlistQuestionInput.ts
+++ apps/backend/src/app/jobs/dto/preShortlistQuestionInput.ts
@@ -3,7 +3,9 @@ import { IsString, MinLength, MaxLength } from 'class-validator';
 export class PreShortlistQuestionInput {
   @IsString()
   @MinLength(5, { message: 'Each question must be at least 5 characters' })
-  @MaxLength(10000, { message: 'Each question must be at most 10000 characters' })
+  @MaxLength(10000, {
+    message: 'Each question must be at most 10000 characters',
+  })
   question!: string;
 
   @IsString()
